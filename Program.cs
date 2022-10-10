d3afddffed88edce0ae134ce31e6fe3b70da93d5
Nicko Albes <mobiemocken0978@gmail.com>
2022-10-10 17:05:45 +0800

    Added Validations to Sign Up Page

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyApplication;
 using MudBlazor.Services;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
